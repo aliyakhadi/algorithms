cba956a8a363fb576d6dd151965d62c896d168d2
aliya <aliya.khadi@gmail.com>
2024-02-21 14:47:41 +0300

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -3,3 +3,6 @@ class Solution:
     return sum(nums[i] + nums[j] < target
                for i in range(len(nums))
                for j in range(i + 1, len(nums)))
+    #unfortunately, that is not a solving of a Week Task which is Prime Sum Problem
+    # you can find it there : https://www.interviewbit.com/problems/prime-sum/ 
+    #
